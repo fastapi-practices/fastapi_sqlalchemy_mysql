3e0bff41030cd67c4b7d0aad150e6d68f0e712ad
wu <jianhengwu0407@gmail.com>
2022-11-22 12:02:54 +0800

    更新返回数据解析

--- backend/app/common/exception/exception_handler.py
+++ backend/app/common/exception/exception_handler.py
@@ -16,9 +16,12 @@ from backend.app.core.conf import settings
 def _get_exception_code(status_code):
     """
     获取返回状态码, OpenAPI, Uvicorn... 可用状态码基于 RFC 定义, 详细代码见下方链接
+
     `python 状态码标准支持 <https://github.com/python/cpython/blob/6e3cc72afeaee2532b4327776501eb8234ac787b/Lib/http
     /__init__.py#L7>`__
+
     `IANA 状态码注册表 <https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml>`__
+
     :param status_code:
     :return:
     """
@@ -43,7 +46,7 @@ def register_exception(app: FastAPI):
         """
         return JSONResponse(
             status_code=_get_exception_code(exc.status_code),
-            content=response_base.fail(code=exc.status_code, msg=exc.detail).dict(),
+            content=response_base.fail(code=exc.status_code, msg=exc.detail),
             headers=exc.headers
         )
 
@@ -58,7 +61,7 @@ def register_exception(app: FastAPI):
         """
         # 常规
         if isinstance(exc, (ValidationError, RequestValidationError)):
-            message = ""
+            message = ''
             data = {}
             for raw_error in exc.raw_errors:
                 if isinstance(raw_error.exc, ValidationError):
@@ -70,16 +73,16 @@ def register_exception(app: FastAPI):
                             data[field_key] = field_title if field_title else field_key
                     for error in exc.errors():
                         field = str(error.get('loc')[-1])
-                        _msg = error.get("msg")
-                        message += f"{data.get(field, field)}{_msg},"
+                        _msg = error.get('msg')
+                        message += f'{data.get(field, field)} {_msg},'
                 elif isinstance(raw_error.exc, json.JSONDecodeError):
                     message += 'json解析失败'
             return JSONResponse(
                 status_code=422,
                 content=response_base.fail(
-                    msg='请求参数非法' if len(message) == 0 else f"请求参数非法, {message[:-1]}",
-                    data={'errors': exc.errors()} if message == "" and settings.UVICORN_RELOAD is True else None
-                ).dict()
+                    msg='请求参数非法' if len(message) == 0 else f"请求参数非法:{message[:-1]}",
+                    data={'errors': exc.errors()} if message == '' and settings.UVICORN_RELOAD is True else None
+                )
             )
 
         # 自定义
@@ -90,12 +93,12 @@ def register_exception(app: FastAPI):
                     code=exc.code,
                     msg=str(exc.msg),
                     data=exc.data if exc.data else None
-                ).dict()
+                )
             )
 
         else:
             return JSONResponse(
                 status_code=500,
-                content=response_base.fail(code=500, msg=str(exc)).dict() if settings.UVICORN_RELOAD else
-                response_base.fail(code=500, msg='Internal Server Error').dict()
+                content=response_base.fail(code=500, msg=str(exc)) if settings.UVICORN_RELOAD else
+                response_base.fail(code=500, msg='Internal Server Error')
             )
--- backend/app/common/response/response_schema.py
+++ backend/app/common/response/response_schema.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from typing import Optional, Any, Union, Set, Dict
 
 from fastapi.encoders import jsonable_encoder
@@ -17,9 +18,14 @@ class ResponseModel(BaseModel):
     """
     统一返回模型, 可以在 FastAPI 接口请求中使用 response_model=ResponseModel 及更多操作, 前提是当它是一个非 200 响应时
     """
-    code: int
-    msg: str
+    code: int = 200
+    msg: str = 'Success'
     data: Optional[Any] = None
+
+    class Config:
+        json_encoders = {
+            datetime: lambda x: x.strftime("%Y-%m-%d %H:%M:%S")
+        }
 
 
 class ResponseBase:
@@ -29,7 +35,7 @@ class ResponseBase:
         return jsonable_encoder(
             data,
             custom_encoder={
-                # datetime: lambda x: x.strftime("%Y-%m-%d %H:%M:%S")  # 格式化时区时间
+                datetime: lambda x: x.strftime("%Y-%m-%d %H:%M:%S")
             }
         )
 
@@ -46,17 +52,20 @@ class ResponseBase:
         :param exclude: 排除返回数据(data)字段
         :return:
         """
-        return ResponseModel(code=code, msg=msg, data=ResponseBase.__encode_json(data)).dict(exclude={'data': exclude})
+        data = data if data is None else ResponseBase.__encode_json(data)
+        return ResponseModel(code=code, msg=msg, data=data).dict(exclude={'data': exclude})
 
     @staticmethod
     @validate_arguments
-    def fail(*, code: int = 400, msg: str = 'Bad Request', data: Any = None):
-        return ResponseModel(code=code, msg=msg, data=data)
+    def fail(*, code: int = 400, msg: str = 'Bad Request', data: Any = None, exclude: Optional[_JsonEncoder] = None):
+        data = data if data is None else ResponseBase.__encode_json(data)
+        return ResponseModel(code=code, msg=msg, data=data).dict(exclude={'data': exclude})
 
     @staticmethod
     @validate_arguments
     def response_200(*, msg: str = 'Success', data: Optional[Any] = None, exclude: Optional[_JsonEncoder] = None):
-        return ResponseModel(code=200, msg=msg, data=ResponseBase.__encode_json(data)).dict(exclude={'data': exclude})
+        data = data if data is None else ResponseBase.__encode_json(data)
+        return ResponseModel(code=200, msg=msg, data=data).dict(exclude={'data': exclude})
 
 
 response_base = ResponseBase()
