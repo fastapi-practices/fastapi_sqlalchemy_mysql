ebeb86ea7e6f55a02985b063a5fb6a15c322bc82
Wu Clan <jianhengwu0407@gmail.com>
2024-04-10 14:32:05 +0800

    修复alembic迁移缺失数据 (#29)

--- app/admin/model/__init__.py
+++ app/admin/model/__init__.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-# 导入所有模型，并将 MappedBase 放在最前面， 以便 MappedBase 拥有它们
-# imported by Alembic
-"""
-
 from common.msd.model import MappedBase
 from app.admin.model.user import User
