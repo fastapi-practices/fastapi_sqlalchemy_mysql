609df01bdc55612cb265bb10133ce18428dbf692
Wu Clan <jianhengwu0407@gmail.com>
2025-01-08 16:50:30 +0800

    清理中间件包冗余代码 (#40)

--- backend/middleware/__init__.py
+++ backend/middleware/__init__.py
@@ -1,27 +1,2 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from fastapi import FastAPI
-
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
-
-from backend.core.conf import settings
-from backend.middleware.access_middle import AccessMiddleware
-
-
-def register_middleware(app: FastAPI) -> None:
-    # cors
-    if settings.MIDDLEWARE_CORS:
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=['*'],
-            allow_credentials=True,
-            allow_methods=['*'],
-            allow_headers=['*'],
-        )
-    # gzip
-    if settings.MIDDLEWARE_GZIP:
-        app.add_middleware(GZipMiddleware)
-    # 接口访问日志
-    if settings.MIDDLEWARE_ACCESS:
-        app.add_middleware(AccessMiddleware)
