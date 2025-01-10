0f17accfed6245768aa31ab1c6e0d9ed0a4ef657
Wu Clan <jianhengwu0407@gmail.com>
2025-01-10 11:55:09 +0800

    更新路由版本配置 (#43)

--- backend/app/admin/api/router.py
+++ backend/app/admin/api/router.py
@@ -4,8 +4,9 @@ from fastapi import APIRouter
 
 from backend.app.admin.api.v1.auth import router as auth_router
 from backend.app.admin.api.v1.user import router as user_router
+from backend.core.conf import settings
 
-v1 = APIRouter()
+v1 = APIRouter(prefix=settings.FASTAPI_API_V1_PATH)
 
 v1.include_router(auth_router)
 v1.include_router(user_router, prefix='/users', tags=['用户'])
--- backend/app/router.py
+++ backend/app/router.py
@@ -3,8 +3,7 @@
 from fastapi import APIRouter
 
 from backend.app.admin.api.router import v1 as admin_v1
-from backend.core.conf import settings
 
 route = APIRouter()
 
-route.include_router(admin_v1, prefix=settings.FASTAPI_API_V1_PATH)
+route.include_router(admin_v1)
